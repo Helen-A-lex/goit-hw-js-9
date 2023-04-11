10bd2aa960f390285f1bc1a2b8055abbdfa5fb5f
Helen-A-lex <117782884+Helen-A-lex@users.noreply.github.com>
2023-04-11 20:20:00 +0000

    Deploying to gh-pages from @ Helen-A-lex/goit-hw-js-9@f85c0bbb101593f0e17cba303998e6b9e842a39b 🚀

---
+++ 01-color-switcher.6787a8c8.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]");console.log(t);var e=document.querySelector("[data-stop]");console.log(e);var o=document.querySelector("body");t.addEventListener("click",(function(){setInterval((function(){var e="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));o.style.backgroundColor=e,t.setAttribute(disabled)}),1e3)})),e.addEventListener("click",(function(){clearInterval(id)}))}();
+//# sourceMappingURL=01-color-switcher.6787a8c8.js.map
