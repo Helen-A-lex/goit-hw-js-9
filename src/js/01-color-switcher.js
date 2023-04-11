f85c0bbb101593f0e17cba303998e6b9e842a39b
Helen-A-lex <elenahomutovskaa652@gmail.com>
2023-04-11 22:19:24 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const buttonStartEl = document.querySelector('[data-start]');
+console.log(buttonStartEl);
+const buttonStopEl = document.querySelector('[data-stop]');
+console.log(buttonStopEl);
+const bodyEl = document.querySelector('body');
+buttonStartEl.addEventListener('click', onBtnStartClick);
+buttonStopEl.addEventListener('click', onBtnStopClick);
+function onBtnStartClick() {
+  const id = setInterval(() => {
+    const newColor = getRandomHexColor();
+    bodyEl.style.backgroundColor = newColor;
+    buttonStartEl.setAttribute(disabled);
+  }, 1000);
+}
+function onBtnStopClick() {
+  clearInterval(id);
+}
